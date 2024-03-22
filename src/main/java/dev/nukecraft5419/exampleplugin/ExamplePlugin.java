6a146afc488e0639983b91d971b4acc43b086b93
Nukecraft5419 <72621737+Nukecraft5419@users.noreply.github.com>
2024-03-22 02:48:38 +0100

    feat: ✨ Improved the main class and properties of gradle

--- src/main/java/dev/nukecraft5419/exampleplugin/ExamplePlugin.java
+++ src/main/java/dev/nukecraft5419/exampleplugin/ExamplePlugin.java
@@ -1,7 +1,9 @@
 package dev.nukecraft5419.exampleplugin;
 
 import dev.nukecraft5419.exampleplugin.api.ExamplePluginAPI;
+import dev.nukecraft5419.exampleplugin.commands.MainCommand;
 import dev.nukecraft5419.exampleplugin.listeners.PlayerJoinListener;
+import dev.nukecraft5419.exampleplugin.utils.MessagesUtils;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.event.Listener;
@@ -16,16 +18,18 @@ public class ExamplePlugin extends JavaPlugin {
         new ExamplePluginAPI(this);
         saveDefaultConfig();
 
-        ExamplePluginAPI.getServer().sendMessage("&eExamplePlugin &asuccessfully loaded!");
+        ExamplePluginAPI.getServer().sendMessage(MessagesUtils.getColorMessage("&eExamplePlugin &asuccessfully enabled!"));
 
         // Register events
         this.addListener(new PlayerJoinListener(this));
 
+        // Register commands
+        getCommand("exampleplugin").setExecutor(new MainCommand(this));
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
-        ExamplePluginAPI.getServer().sendMessage("&eExamplePlugin &asuccessfully disabled!");
+        ExamplePluginAPI.getServer().sendMessage(MessagesUtils.getColorMessage("&eExamplePlugin &cwas successfully disabled!"));
     }
 }
